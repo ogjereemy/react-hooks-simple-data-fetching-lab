7f1ab9b4634d70ecb06b686f0b455b80808cbea2
ogola <ogolajeremy601@gmail.com>
2024-04-30 11:52:04 +0300

    adds data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import { useEffect, useState } from "react";
+
+function App() {
+ const [dogs, setDogs] = useState(null)
+ const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data =>{
+            setDogs(data.message)
+            setLoading(false)
+        })
+    }, [])  
+
+    if (loading) {
+        return <p>Loading...</p>;
+      } else {
+        return <img src={dogs} alt="A Random Dog" />;
+      }
+    
+}
+
+export default App;
